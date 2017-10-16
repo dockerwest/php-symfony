609305ce50ab069873a85bee5f78cea720464db9
Tom Van Herreweghe <tom@theanalogguy.be>
2017-10-16 12:05:03 +0200

    Symfony3 requires Make utility

--- 7.2/scripts/install.sh
+++ 7.2/scripts/install.sh
@@ -7,6 +7,8 @@ apt-get update
 apt-get install -y php7.2-gd php7.2-mysql php7.2-sqlite3 php7.2-bz2 \
     php7.2-xml php7.2-mbstring php7.2-zip php-redis php-imagick php-json \
     php-bcmath php7.2-intl
+    
+apt-get install -y make
 
 apt-get clean -y
 
